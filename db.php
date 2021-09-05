d9de32973dbb204ea8697ac51e228fa3fc3b7564
IrmaRamirez <iriraga@hotmail.com>
2021-09-05 18:20:16 -0500

    Creación de Tabla para manipular Base de Datos
    Se hace uso de las técnicas de Php para generar código Html con el fin de generar una tabla con los valores de los datos que existen en la Base de Datos, al tiempo que se desarrollan consultoas para ingresar y eliminar datos.

--- db.php
+++ db.php
@@ -1,13 +1,18 @@
 <?php
+
+// Generar sesiones para guardar datos de la sesión de ingreso de datos
+session_start();
+
+// Realizar coneccion entre la base de datos xampp - mysql y el proyecto
 $conn = mysqli_connect(
-    'localhost',
-    'root',
-    '',
-    'php_mysql_crud'
+    'localhost',        // Host
+    'root',             // User
+    '',                 // password
+    'php_mysql_crud'    // nombre de la base de datos
 );
 
-if (isset($conn))
-{
+// Si la conección es correcta:
+if (isset($conn)){
     echo 'DB is connected, pero hay que asegurar de abrir Xampp y ejecutar el módulo MySQL';
 }
 ?>
---
+++ save_task.php
@@ -0,0 +1,32 @@
+ <!-- Al presionar el botón de enviar pasará todo esto: -->
+<?php
+include("db.php");
+if (isset($_POST['save_task'])) {
+    echo 'saving';
+    // Variables de la Base de Datos:
+    $title = $_POST['title'];
+    $description = $_POST['description'];
+
+    echo $title;
+    echo $description;
+
+    // QUERY: Insertar nuevo elemento a la Base de Datos.
+    $query = "INSERT INTO task(title, description) VALUES ('$title', '$description')";
+    $result = mysqli_query($conn, $query);
+    // Confirmación de nuevo elemento ingresado a la Base de Datos.
+    if (!$result) {
+        die("Query Failed");
+    }
+
+    // Manejo de Sesiones para el ingreso de Datos al Formulario Previo a la Base de Datos.
+    $_SESSION['message'] = 'Task Saved SuCceSfuLly';
+
+    // Definir el color de la ventana de mensaje que aparece cuando se ha presionado el botón de guardar:
+    // - success: verde
+    // - danger: rojo
+    // - alert : amarillo
+    $_SESSION['message_type'] = 'success';
+
+    // Al terminar de ejecutarse el botón de enviar, se cargará nuevamente la ventana de "index.php"
+    header("Location: index.php");
+}
